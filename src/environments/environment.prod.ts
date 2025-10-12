793580eb2ab83abeb63a69250df655ffab9ad370
Mohamed Wasim <mohamedwasim972000@gmail.com>
2025-10-12 15:11:28 +0530

    updated prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'spidertodo-api-h7hnbja3h9gjcfcf.centralindia-01.azurewebsites.net'
+  apiBaseUrl: 'https://spidertodo-api-h7hnbja3h9gjcfcf.centralindia-01.azurewebsites.net'
 };
