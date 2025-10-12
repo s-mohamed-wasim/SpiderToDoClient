d7844cf44ee653b795f9e8368fd4a536f0bc35ad
Mohamed Wasim <mohamedwasim972000@gmail.com>
2025-10-12 15:35:15 +0530

    corrected production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://spidertodo-api-h7hnbja3h9gjcfcf.centralindia-01.azurewebsites.net'
+  apiBaseUrl: 'https://spidertodo-api-h7hnbja3h9gjcfcf.centralindia-01.azurewebsites.net/api'
 };
